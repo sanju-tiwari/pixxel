571206398e9d34ec3fb79127ae4e4da1d55216dc
Sanju Tiwari <tiwarisanju923@gmail.com>
2025-09-08 12:42:50 +0530

    Updated project with new changes

--- src/app/(main)/editor/[projectId]/_component/editor-top-bar.jsx
+++ src/app/(main)/editor/[projectId]/_component/editor-top-bar.jsx
@@ -225,20 +225,7 @@ return(
                  <h1 className="font-extrabold capitalize">{project.title}</h1>
                  <div className=' flex items-center gap-3'>
 
-                  <Button variant='outline' size="sm" onClick={handleResertToOrigin} disabled={isSaving || !project.originalImageUrl} className='gap-2'>
-                     {isSaving ? (
-                        <>
-                         <Loader2 className='h-4 w-4 animate-spin'/>
-                         Saving...
-                         </>
-                     ):(
-                      <>
-                       <Save className='h-4 w-4'/>
-                       Save
-                      </>
-                     )
-                    }
-                    </Button>
+          
                        <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
